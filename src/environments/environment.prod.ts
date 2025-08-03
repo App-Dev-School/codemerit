aa1c250bac7ee7c22cf59debcc3527fe6f0049d5
Vishal Kumar <javacheartofmine@gmail.com>
2025-08-04 03:08:54 +0530

    prod api url https change

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,9 @@
 export const environment = {
   APP_DATE_FORMAT : 'd-MMM-y',
   production: true,
-   apiUrl: "http://apis.appdevops.in/",
-   uploadsUrl: "http://apis.appdevops.in/uploads/",
+   //apiUrl: "http://apis.appdevops.in/",
+   apiUrl: "https://apis.appdevops.in/",
+   uploadsUrl: "https://apis.appdevops.in/uploads/",
    stripe: {
     publicKey: 'pk_test_LHMHo4FCtltdVijsCnSjiN8X00Qa33WfAw',
   }
