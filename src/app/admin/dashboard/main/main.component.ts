f9fd325c157737ad8c3c011a8ad10e99ba61acba
Vishal Kumar <javacheartofmine@gmail.com>
2025-10-23 20:57:17 +0530

    Added Time Series Graph in Admin Dashboard

--- src/app/admin/dashboard/main/main.component.ts
+++ src/app/admin/dashboard/main/main.component.ts
@@ -11,27 +11,38 @@ import { AuthService } from '@core';
 import { InitialRole } from '@core/models/initial-role.data';
 import { MasterService } from '@core/service/master.service';
 import { SnackbarService } from '@core/service/snackbar.service';
+import { TimeframeData, TimeseriesChartComponent } from '@shared/components/timeseries-chart/timeseries-chart.component';
 import { AdminDashboardData } from '../../dtos/admin-dashboard.model';
-import { TimeseriesChartComponent } from '@shared/components/timeseries-chart/timeseries-chart.component';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { FormsModule } from "@angular/forms";
+
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.scss'],
   imports: [
-    //RouterLink,
     MatCardModule,
     MatButtonModule,
+    MatButtonToggleModule,
     MatTableModule,
     MatSelectModule,
     MatMenuModule,
     MatIconModule,
-    TimeseriesChartComponent
+    TimeseriesChartComponent,
+    FormsModule
   ]
 })
 export class MainComponent implements OnInit {
+  currentModule: string;
   selectedTimePeriod: string = 'Monthly';
   public initialRoles: InitialRole[] = AuthConstants.CURRENT_ROLE_OPTIONS;
   dashboard: AdminDashboardData;
+  timeframeData: any = {};
+  usersTimeframe: any;
+  quizzesTimeframe: any;
+  attemptsTimeframe: any;
+  questionsTimeframe: any;
+
   subject = "";
   title = 'LMS Stat';
   subtitle = 'LSMS Resource Overview';
@@ -46,30 +57,11 @@ export class MainComponent implements OnInit {
   }
 
   ngOnInit() {
+    this.currentModule = 'Users';
     this.takeRouteParams();
     this.loadAdminDashboard();
   }
 
-  loadAdminDashboard() {
-    this.masterService.getAdminDashboard()
-      .subscribe({
-        next: (response) => {
-          console.log('response:', response);
-          //this.submitted = false;
-          if (response && !response.error) {
-            this.dashboard = response?.data;
-          } else {
-            this.loading = false;
-            this.snackService.display('snackbar-dark', response?.message ?? "Error fetching dashboard.", 'bottom', 'center');
-          }
-        },
-        error: (error) => {
-          this.loading = false;
-          this.snackService.display('snackbar-dark', 'Error enrolling subject. Please try again.', 'bottom', 'center');
-          console.error('Enroll Subject API Error:', error);
-        },
-      });
-  }
   //Implement for admin
   takeRouteParams() {
     const subject = this.route.snapshot.paramMap.get('subject');
@@ -114,4 +106,91 @@ export class MainComponent implements OnInit {
   viewAttempts() {
     this.router.navigate(['/app/subscription']);
   }
+
+  private transformTimeSeriesData(
+    title: string,
+    dailyData: { date: string; count: number }[],
+    weeklyData: { week: string; count: number }[]
+  ): TimeframeData {
+    const daily = dailyData.map(item => ({
+      key: item.date,
+      value: item.count,
+    }));
+
+    const weekly = weeklyData.map(item => ({
+      key: item.week,
+      value: item.count,
+    }));
+
+    return { title, daily, weekly };
+  }
+
+  
+  loadAdminDashboard() {
+    this.masterService.getAdminDashboard()
+      .subscribe({
+        next: (response) => {
+          console.log('response:', response);
+          //this.submitted = false;
+          if (response && !response.error) {
+            this.dashboard = response?.data;
+            //Form the analytics data
+            if (this.dashboard) {
+              this.usersTimeframe = this.transformTimeSeriesData(
+                'Users',
+                this.dashboard.timeSeries.daily.users,
+                this.dashboard.timeSeries.weekly.users
+              );
+
+              this.questionsTimeframe = this.transformTimeSeriesData(
+                'Questions',
+                this.dashboard.timeSeries.daily.questions,
+                this.dashboard.timeSeries.weekly.questions
+              );
+
+              this.quizzesTimeframe = this.transformTimeSeriesData(
+                'Quizzes',
+                this.dashboard.timeSeries.daily.quizzes,
+                this.dashboard.timeSeries.weekly.quizzes
+              );
+
+              this.attemptsTimeframe = this.transformTimeSeriesData(
+                'Attempts',
+                this.dashboard.timeSeries.daily.attempts,
+                this.dashboard.timeSeries.weekly.attempts
+              );
+              this.onModuleChange({value: this.currentModule});
+            }
+          } else {
+            this.loading = false;
+            this.snackService.display('snackbar-dark', response?.message ?? "Error fetching dashboard.", 'bottom', 'center');
+          }
+        },
+        error: (error) => {
+          this.loading = false;
+          this.snackService.display('snackbar-dark', 'Error enrolling subject. Please try again.', 'bottom', 'center');
+          console.error('Enroll Subject API Error:', error);
+        },
+      });
+  }
+
+  onModuleChange(event: any) {
+    console.log('Selected module:', event.value);
+    this.currentModule = event.value;
+    switch (this.currentModule) {
+      case "Users":
+        this.timeframeData = this.usersTimeframe;
+        break;
+      case "Quizzes":
+        this.timeframeData = this.quizzesTimeframe;
+        break;
+      case "Attempts":
+        this.timeframeData = this.attemptsTimeframe;
+        break;
+      case "Questions":
+        this.timeframeData = this.questionsTimeframe;
+        break;
+    }
+  }
+
 }
--- src/app/shared/components/timeseries-chart/timeseries-chart.component.ts
+++ src/app/shared/components/timeseries-chart/timeseries-chart.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { JsonPipe } from '@angular/common';
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardHeader, MatCardModule } from '@angular/material/card';
@@ -20,12 +21,19 @@ export type ChartOptions = {
   colors: string[];
 };
 
+export type TimeframeData = {
+  title: string;
+  daily: { key: string; value: number }[];
+  weekly: { key: string; value: number }[];
+};
+
 @Component({
   selector: 'app-timeseries-chart',
   templateUrl: './timeseries-chart.component.html',
   styleUrls: ['./timeseries-chart.component.css'],
   imports: [
     FormsModule,
+    JsonPipe,
     ReactiveFormsModule,
     NgApexchartsModule,
     MatFormField,
@@ -35,12 +43,20 @@ export type ChartOptions = {
     MatSelectModule
   ]
 })
-export class TimeseriesChartComponent implements OnInit {
-  selectedTimePeriod: string = 'Monthly';
+export class TimeseriesChartComponent implements OnInit, OnChanges {
+  selectedTimePeriod: string = 'Daily';
   public areaChartOptions!: Partial<ChartOptions>;
+  @Input() timeframe: any;
 
   ngOnInit() {
+    //console.log("timeframe", this.timeframe);
     this.chart1();
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes['timeframe'] && !changes['timeframe'].firstChange) {
+      this.chart1(); // re-render whenever timeframe changes
+    }
   }
 
   onTimePeriodChange(event: any): void {
@@ -48,104 +64,47 @@ export class TimeseriesChartComponent implements OnInit {
   }
 
   private chart1() {
-    let categories: string[] = [];
-    let newPatientsData: number[] = [];
-    let oldPatientsData: number[] = [];
+  if (!this.timeframe) return;
+  let categories: string[] = [];
+  let values: number[] = [];
 
-    switch (this.selectedTimePeriod) {
-      case 'Daily':
-        categories = [
-          '2024-11-01',
-          '2024-11-02',
-          '2024-11-03',
-          '2024-11-04',
-          '2024-11-05',
-          '2024-11-06',
-          '2024-11-07',
-        ];
-        newPatientsData = [5, 8, 6, 4, 7, 9, 10];
-        oldPatientsData = [2, 3, 4, 3, 5, 6, 7];
-        break;
-
-      case 'Monthly':
-        categories = [
-          '2024-01',
-          '2024-02',
-          '2024-03',
-          '2024-04',
-          '2024-05',
-          '2024-06',
-          '2024-07',
-        ];
-        newPatientsData = [31, 40, 28, 51, 42, 85, 77];
-        oldPatientsData = [11, 32, 45, 32, 34, 52, 41];
-        break;
-
-      case 'Yearly':
-        categories = ['2018', '2019', '2020', '2021', '2022', '2023', '2024'];
-        newPatientsData = [200, 250, 300, 450, 600, 700, 800];
-        oldPatientsData = [120, 180, 200, 300, 400, 500, 600];
-        break;
-    }
-
-    this.areaChartOptions = {
-      series: [
-        {
-          name: 'New Patients',
-          data: newPatientsData,
-        },
-        {
-          name: 'Old Patients',
-          data: oldPatientsData,
-        },
-      ],
-      chart: {
-        height: 300,
-        type: 'area',
-        toolbar: {
-          show: false,
-        },
-        foreColor: '#9aa0ac',
-      },
-      colors: ['#407fe4', '#E47E3C'],
-      dataLabels: {
-        enabled: false,
-      },
-      grid: {
-        show: true,
-        borderColor: '#9aa0ac',
-        strokeDashArray: 1,
-      },
-      stroke: {
-        curve: 'smooth',
-      },
-      xaxis: {
-        type: 'datetime',
-        categories: categories,
-        labels: {
-          show: true,
-          offsetX: 20,
-          offsetY: 0,
-        },
-      },
-      legend: {
-        show: true,
-        position: 'top',
-        horizontalAlign: 'center',
-        offsetX: 0,
-        offsetY: 0,
-      },
-
-      tooltip: {
-        theme: 'dark',
-        marker: {
-          show: true,
-        },
-        x: {
-          show: true,
-        },
-      },
-    };
+  if (this.selectedTimePeriod === 'Daily' && this.timeframe.daily) {
+    categories = this.timeframe.daily.map((item: any) => item.key);
+    values = this.timeframe.daily.map((item: any) => item.value);
+  } else if (this.selectedTimePeriod === 'Weekly' && this.timeframe.weekly) {
+    categories = this.timeframe.weekly.map((item: any) => item.key);
+    values = this.timeframe.weekly.map((item: any) => item.value);
   }
+
+  this.areaChartOptions = {
+    series: [
+      {
+        name: this.timeframe.title,
+        data: values,
+      },
+    ],
+    chart: {
+      height: 300,
+      type: 'bar',
+      toolbar: { show: false },
+      foreColor: '#9aa0ac',
+    },
+    colors: ['#407fe4'],
+    dataLabels: { enabled: false },
+    grid: {
+      show: true,
+      borderColor: '#9aa0ac',
+      strokeDashArray: 1,
+    },
+    stroke: { curve: 'smooth' },
+    xaxis: {
+      type: 'category',
+      categories,
+      labels: { show: true },
+    },
+    legend: { show: true, position: 'top' },
+    tooltip: { theme: 'dark' },
+  };
+}
 
 }
