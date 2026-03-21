9b39026cf5179b66a120f92c294b0fd28bda1d46
App Dev <appdevschool@gmail.com>
2026-03-22 03:49:48 +0530

    Certificate enhancements

--- src/app/shared/components/certificate/certificate.model.ts
+++ src/app/shared/components/certificate/certificate.model.ts
@@ -1,15 +1,27 @@
+export enum CertificateTemplateId {
+  InternshipCompletion = 'Internship Completion',
+  WorkExperience = 'Work Experience',
+  Appreciation = 'Appreciation',
+  MilestoneCompletion = 'Milestone Completion'
+}
+
 export interface CertificateModel {
+  userId?: number;
   userName: string;
   skillName: string;
   tierDisplayName: string;
   assessmentTitle: string;
   scorePercentage: number;
   certificateNumber: string;
-  templateId: string;
+  issuerName: string;
+  issuerLogo: string;
+  sponsorName: string;
+  sponsorLogo: string;
   issuedDate: Date;
   expiryDate: Date;
   verificationCode: string;
+  templateId: CertificateTemplateId;
   platformName: string;
-  issuerName: string;
+  signedBy: string;
   programLead: string;
 }
