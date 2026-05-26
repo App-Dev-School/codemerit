30168ce5fb30473c1363f3de349f56ee9bcc7ed1
App Dev <appdevschool@gmail.com>
2026-05-27 00:30:44 +0530

    Redesigned quiz thumbnail and removed old design

--- src/app/shared/components/quiz-thumbnail/quiz-thumbnail.component.ts
+++ src/app/shared/components/quiz-thumbnail/quiz-thumbnail.component.ts
@@ -1,26 +1,27 @@
 import { CommonModule } from '@angular/common';
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatIcon } from '@angular/material/icon';
 
 @Component({
-  selector: 'app-quiz-thumbnail',
-  templateUrl: './quiz-thumbnail.component.html',
-  styleUrls: ['./quiz-thumbnail.component.scss'],
-   imports: [
-    CommonModule,
-    MatCardModule,
-    MatButtonModule,
-    FormsModule,
-    MatIcon
-  ],
+    selector: 'app-quiz-thumbnail',
+    templateUrl: './quiz-thumbnail.component.html',
+    styleUrls: ['./quiz-thumbnail.component.scss'],
+    imports: [
+        CommonModule,
+        MatCardModule,
+        MatButtonModule,
+        FormsModule,
+        MatIcon
+    ],
 })
 export class QuizThumbnailComponent {
-  @Input() quiz: any;
+    @Input() quiz: any;
+    @Output() quizSelected = new EventEmitter<string>();
 
-  takeQuiz(slug: string) {
-
-  }
+    takeQuiz(slug: string) {
+        this.quizSelected.emit(slug);
+    }
 }
