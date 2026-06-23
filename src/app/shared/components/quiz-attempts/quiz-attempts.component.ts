fc16187e5d5db8bfb5466ecbd5a134121915483b
App Dev <appdevschool@gmail.com>
2026-06-23 23:34:54 +0530

    Updated signup page + code cleanup

--- src/app/shared/components/quiz-attempts/quiz-attempts.component.ts
+++ src/app/shared/components/quiz-attempts/quiz-attempts.component.ts
@@ -3,39 +3,17 @@ import { CommonModule, NgClass } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
-import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-import { Swiper } from 'swiper';
-import { register } from 'swiper/element/bundle';
 
 @Component({
   selector: 'app-quiz-attempts',
   standalone: true,
   imports: [CommonModule, MatButtonModule, MatCardModule, MatIconModule, NgClass],
   templateUrl: './quiz-attempts.component.html',
-  styleUrls: ['./quiz-attempts.component.scss'],
-  schemas: [CUSTOM_ELEMENTS_SCHEMA]
+  styleUrls: ['./quiz-attempts.component.scss']
 })
-export class QuizAttemptsComponent implements AfterViewInit {
+export class QuizAttemptsComponent  {
   @Input() questions: Array<any> = [];
-  @ViewChild('swiperEx') swiperEx!: ElementRef<{ swiper: Swiper }>;
-  currentQuestionId = 0;
-
-  constructor() {
-    register();
-  }
-
-  ngAfterViewInit(): void {
-    if (this.questions?.length) {
-      this.currentQuestionId = 0;
-    }
-  }
-
-  private updateCurrentIndex(): void {
-    if (this.swiperEx?.nativeElement?.swiper) {
-      this.currentQuestionId = this.swiperEx.nativeElement.swiper.activeIndex;
-    }
-  }
-
+ 
   getStatusLabel(question: any): string {
     if (question.isSkipped === true) {
       return 'Skipped';
