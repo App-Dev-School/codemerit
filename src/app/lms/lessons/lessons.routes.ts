ea23b905fad98aac6d603557b61f8623bdfada73
carolinjoannasherylk <carolinjoannasheryl@gmail.com>
2026-06-30 00:21:14 +0530

    Lesson Listing in Admin Dashboard

---
+++ src/app/lms/lessons/lessons.routes.ts
@@ -0,0 +1,16 @@
+import { Route } from '@angular/router';
+import { Page404Component } from '../../authentication/page404/page404.component';
+import { ManageLessonsComponent } from './manage-lessons/manage-lessons.component';
+
+export const LESSONS_ROUTE: Route[] = [
+  {
+    path: '',
+    redirectTo: 'list',
+    pathMatch: 'full',
+  },
+  {
+    path: 'list',
+    component: ManageLessonsComponent,
+  },
+  { path: '**', component: Page404Component },
+];
